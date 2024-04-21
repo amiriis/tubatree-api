5cfaee66dfbc17bae0f8b7365142824095ac5533
Amirhossein Mahmoodi <amir.mahmoodi74@gmail.com>
2024-04-21 16:14:49 +0330

    update new

--- src/allah/allah.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AllahService } from './allah.service';
-
-@Controller('allah')
-export class AllahController {
-  constructor(private readonly allahService: AllahService) {}
-}
--- src/allah/allah.module.ts
+++ src/allah/allah.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AllahService } from './allah.service';
-import { AllahController } from './allah.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AllahService } from './allah.service';
 import { AllahNames } from './entities/allah.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AllahNames])],
-  controllers: [AllahController],
+  controllers: [],
   providers: [AllahService],
 })
 export class AllahModule {}
